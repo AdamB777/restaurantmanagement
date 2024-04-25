a06a1204fee2e9ece87dbb432ae96a90189b7973
kaszakinga90 <kaszakinga90@gmail.com>
2024-04-25 16:51:58 +0200

    identity added

--- Restaurant_Reservations/Restaurant_Reservations_Api/Api/Controllers/BaseApiController.cs
+++ Restaurant_Reservations/Restaurant_Reservations_Api/Api/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Core;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers
 {
@@ -6,5 +8,35 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
+        protected readonly IMediator _mediator;
+
+        public BaseApiController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSucces && result.Value != null)
+                return Ok(result.Value);
+            if (result.IsSucces && result.Value == null)
+                return NotFound();
+            return BadRequest(result.Error);
+        }
+
+        //protected ActionResult HandlePagedResult<T>(Result<PagedList<T>> result)
+        //{
+        //    if (result == null) return NotFound();
+        //    if (result.IsSucces && result.Value != null)
+        //    {
+        //        Response.AddPaginationHeader(result.Value.CurrentPage, result.Value.PageSize, result.Value.TotalCount, result.Value.TotalPages);
+        //        return Ok(result.Value);
+        //    }
+
+        //    if (result.IsSucces && result.Value == null)
+        //        return NotFound();
+        //    return BadRequest(result.Error);
+        //}
     }
 }
--- Restaurant_Reservations/Restaurant_Reservations_Api/RestaurantDB/RestaurantContext.cs
+++ Restaurant_Reservations/Restaurant_Reservations_Api/RestaurantDB/RestaurantContext.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using ModelsDB;
+using ModelsDB.SystemUsers;
 using System.Diagnostics;
 
 namespace RestaurantDB
@@ -10,9 +14,48 @@ namespace RestaurantDB
             
         }
 
+        public DbSet<Customer> CustomersDB { get; set; }
+        public DbSet<Employee> EmployeesDB { get; set; }
+        public DbSet<Owner> OwnersDB { get; set; }
+        public DbSet<SuperAdmin> SuperAdminsDB { get; set; }
+        public DbSet<Address> AddressesDB { get; set; }
+        public DbSet<CountryState> CountryStatesDB { get; set; }
+        public DbSet<EmployeeAddress> EmployeeAddressesDB { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+               .HasIndex(u => new { u.FirstName, u.LastName })
+               .HasDatabaseName("IX_FirstNameLastName");
+            modelBuilder.Entity<IdentityRole>()
+                .HasData(
+                new IdentityRole { Name = "SuperAdmin", NormalizedName = "SUPERADMIN" },
+                new IdentityRole { Name = "Owner", NormalizedName = "OWNER" },
+                new IdentityRole { Name = "Employee", NormalizedName = "EMPLOYEE" },
+                new IdentityRole { Name = "Customer", NormalizedName = "CUSTOMER" }
+                );
+            modelBuilder.Entity<IdentityUserLogin<int>>(b =>
+            {
+                b.HasKey(login => new { login.ProviderKey, login.LoginProvider });
+            });
+            modelBuilder.Entity<IdentityUserRole<int>>(b =>
+            {
+                b.HasKey(ur => new { ur.UserId, ur.RoleId });
+            });
+            modelBuilder.Entity<IdentityUserToken<int>>(b =>
+            {
+                b.HasKey(ut => new { ut.UserId, ut.LoginProvider, ut.Name });
+            });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.LogTo(item => Debug.WriteLine(item));
+            // DONE : do usunięcia w wersji produkcyjnej
+            optionsBuilder.ConfigureWarnings(w => w.Ignore(SqlServerEventId.SavepointsDisabledBecauseOfMARS));
             base.OnConfiguring(optionsBuilder);
         }
     }
